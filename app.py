4874366f9959bf3c223f7bddb1626403e25e1b8a
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-03-08 17:17:09 -0800

    finished question.html

--- app.py
+++ app.py
@@ -9,4 +9,16 @@ app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 
-@app
+@app.get('/')
+def questions():
+
+    return render_template("questions.html",
+                        # use silly_story
+                            inputs = silly_story.prompts
+                           )
+
+@app.get('/results')
+def results():
+
+    results = request.args.get(input)
+    return render_template("results.html")
